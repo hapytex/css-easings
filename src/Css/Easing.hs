90a0331134965beadf34cb5678f9f294b1c76719
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-03-20 12:34:23 +0100

    documentation for the PostCSS easing patterns

--- src/Css/Easing.hs
+++ src/Css/Easing.hs
@@ -107,75 +107,99 @@ instance Default JumpTerm where
     def = JumpNone
 
 -- PostCSS
+-- | A pattern that defines the PostCSS easing pattern @easeInSine@.
 pattern EaseInSine :: Easing
 pattern EaseInSine = CubicBezier 0.12 0 0.39 0
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutSine@.
 pattern EaseOutSine :: Easing
 pattern EaseOutSine = CubicBezier 0.61 1 0.88 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutSine@.
 pattern EaseInOutSine :: Easing
 pattern EaseInOutSine = CubicBezier 0.37 0 0.63 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInQuad@.
 pattern EaseInQuad :: Easing
 pattern EaseInQuad = CubicBezier 0.11 0 0.5 0
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutQuad@.
 pattern EaseOutQuad :: Easing
 pattern EaseOutQuad = CubicBezier 0.5 1 0.89 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutQuad@.
 pattern EaseInOutQuad :: Easing
 pattern EaseInOutQuad = CubicBezier 0.45 0 0.55 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInCubic@.
 pattern EaseInCubic :: Easing
 pattern EaseInCubic = CubicBezier 0.32 0 0.67 0
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutCubic@.
 pattern EaseOutCubic :: Easing
 pattern EaseOutCubic = CubicBezier 0.33 1 0.68 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutCubic@.
 pattern EaseInOutCubic :: Easing
 pattern EaseInOutCubic = CubicBezier 0.65 0 0.35 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInQuart@.
 pattern EaseInQuart :: Easing
 pattern EaseInQuart = CubicBezier 0.5 0 0.75 0
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutQuart@.
 pattern EaseOutQuart :: Easing
 pattern EaseOutQuart = CubicBezier 0.25 1 0.5 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutQuart@.
 pattern EaseInOutQuart :: Easing
 pattern EaseInOutQuart = CubicBezier 0.76 0 0.24 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInQuint@.
 pattern EaseInQuint :: Easing
 pattern EaseInQuint = CubicBezier 0.64 0 0.78 0
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutQuint@.
 pattern EaseOutQuint :: Easing
 pattern EaseOutQuint = CubicBezier 0.22 1 0.36 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutQuint@.
 pattern EaseInOutQuint :: Easing
 pattern EaseInOutQuint = CubicBezier 0.83 0 0.17 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInExpo@.
 pattern EaseInExpo :: Easing
 pattern EaseInExpo = CubicBezier 0.7 0 0.84 0
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutExpo@.
 pattern EaseOutExpo :: Easing
 pattern EaseOutExpo = CubicBezier 0.16 1 0.3 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutExpo@.
 pattern EaseInOutExpo :: Easing
 pattern EaseInOutExpo = CubicBezier 0.87 0 0.13 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInCirc@.
 pattern EaseInCirc :: Easing
 pattern EaseInCirc = CubicBezier 0.55 0 1 0.45
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutCirc@.
 pattern EaseOutCirc :: Easing
 pattern EaseOutCirc = CubicBezier 0 0.55 0.45 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutCirc@.
 pattern EaseInOutCirc :: Easing
 pattern EaseInOutCirc = CubicBezier 0.85 0 0.15 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInBack@.
 pattern EaseInBack :: Easing
 pattern EaseInBack = CubicBezier 0.36 0 0.66 (-0.56)
 
+-- | A pattern that defines the PostCSS easing pattern @easeOutBack@.
 pattern EaseOutBack :: Easing
 pattern EaseOutBack = CubicBezier 0.34 1.56 0.64 1
 
+-- | A pattern that defines the PostCSS easing pattern @easeInOutBack@.
 pattern EaseInOutBack :: Easing
 pattern EaseInOutBack = CubicBezier 0.68 (-0.6) 0.32 1.6
 
